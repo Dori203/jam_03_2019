51cd9021868925ab7c4de18d8531049045210a07
Dori Rosenberg <33037430+Dori203@users.noreply.github.com>
2021-01-05 12:09:07 +0200

    updated basic camera features

--- Assets/Scripts/TopCameraFollow.cs
+++ Assets/Scripts/TopCameraFollow.cs
@@ -6,11 +6,11 @@ public class TopCameraFollow : MonoBehaviour
 {
 
     [SerializeField] private Transform player;
+    [SerializeField] private Vector3 offset;
 
     private void LateUpdate()
     {
-        Vector3 newPosition = player.position;
-        newPosition.y = transform.position.y;
+        Vector3 newPosition = player.position + offset;
         transform.position = newPosition;
      }
 }
