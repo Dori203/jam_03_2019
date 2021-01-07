0be88279a7bd1e5984a94ae0a1d90ed23a0a9a1b
idanro <idan.rooze@gmail.com>
2021-01-07 18:23:04 +0200

    Landmarks + minimap update

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private Quaternion rotationOffset;
+    [SerializeField] private bool followRotation;
+    private void LateUpdate()
+    {
+        Vector3 newPosition = player.position + positionOffset;
+        transform.position = newPosition;
+        if (followRotation)
+        {
+            transform.rotation = player.rotation * rotationOffset;
+        }
+    }
+}
