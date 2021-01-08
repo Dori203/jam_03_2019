384e173ca5631b0343d670a64fa9c214c238c1fd
idanro <idan.rooze@gmail.com>
2021-01-08 19:48:15 +0200

    Restrict rock spawn area

--- Assets/SpawnRocks.cs
+++ Assets/SpawnRocks.cs
@@ -10,22 +10,21 @@ public class SpawnRocks : MonoBehaviour
     public float rockMinScale = 1f;
     public float rockMaxScale = 5f;
     public float heightVariation = 2f;
+    public float restrictAreaSize = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i< numberOfRocks; i++)
+        for (int i = 0; i < numberOfRocks; i++)
         {
             Vector3 position = new Vector3((Random.value - 0.5f) * squareLength, (Random.value - 0.5f) * heightVariation, (Random.value - 0.5f) * squareLength);
+            while ((position.x <= restrictAreaSize && position.x >= -restrictAreaSize) && (position.z <= restrictAreaSize && position.z >= -restrictAreaSize))
+            {
+                position = new Vector3((Random.value - 0.5f) * squareLength, (Random.value - 0.5f) * heightVariation, (Random.value - 0.5f) * squareLength);
+            }
             GameObject rockType = rocks[Random.Range(0, rocks.Length)];
-            GameObject currentRock = Instantiate(rockType, position, Random.rotation,gameObject.transform);
+            GameObject currentRock = Instantiate(rockType, position, Random.rotation, gameObject.transform);
             float rockScale = Random.Range(rockMinScale, rockMaxScale);
-            currentRock.transform.localScale = new Vector3(rockScale,rockScale,rockScale);
+            currentRock.transform.localScale = new Vector3(rockScale, rockScale, rockScale);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
