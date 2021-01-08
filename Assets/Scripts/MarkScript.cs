5eca3d344c5db025b7498361e193411db4849571
idanro <idan.rooze@gmail.com>
2021-01-08 18:09:03 +0200

    Map Mark script (static for now)

---
+++ Assets/Scripts/MarkScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkScript : MonoBehaviour
+{
+    [SerializeField] Sprite iconSprite;
+    [SerializeField] private float iconScale;
+    private SpriteRenderer iconRenderer;
+    private GameObject icon;
+    private Vector3 iconlocalRotation;
+    private void Start()
+    {
+        icon = new GameObject("rock Mark");
+        icon.transform.position = transform.position;
+        icon.transform.Rotate(new Vector3(90f,0f,0f));
+        icon.transform.localScale = new Vector3(iconScale, iconScale, iconScale);
+        icon.layer = LayerMask.NameToLayer("Map");
+        iconRenderer = icon.AddComponent<SpriteRenderer>();
+        iconRenderer.sprite = iconSprite;
+        icon.transform.SetParent(this.transform);
+    }
+}
