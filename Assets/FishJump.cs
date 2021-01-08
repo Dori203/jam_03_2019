b6399e4e70dc63f39c200bc98e2c10edbdd50024
idanro <idan.rooze@gmail.com>
2021-01-08 03:32:10 +0200

    Fish Box and stuff

---
+++ Assets/FishJump.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishJump : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float timerMin;
+    [SerializeField] private float timerMax;
+    [SerializeField] private float forceFactor;
+    [SerializeField] private float torqueFactor;
+
+    private float timer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Random.Range(timerMin, timerMax);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            rb.AddForce(new Vector3(Random.Range(-1f, 1f), Random.Range(5f, 7.5f), Random.Range(-1f, 1f)).normalized * forceFactor,ForceMode.Impulse);
+            rb.AddTorque(new Vector3(Random.Range(-1f, 1f), Random.Range(-1, 1f), Random.Range(-1f, 1f)).normalized * torqueFactor, ForceMode.Impulse);
+            timer = timer = Random.Range(timerMin, timerMax);
+        }
+    }
+}
+
