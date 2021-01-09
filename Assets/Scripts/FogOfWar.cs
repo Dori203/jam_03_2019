5e78609bafade8351fd904a193de23ba4b7be699
Dori Rosenberg <33037430+Dori203@users.noreply.github.com>
2021-01-09 15:49:38 +0200

    Added fog of war
    Changed the script for fogOfWar to create Probuilder with script.

--- Assets/Scripts/FogOfWar.cs
+++ Assets/Scripts/FogOfWar.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ProBuilder;
+using UnityEngine.ProBuilder.MeshOperations;
 
 public class FogOfWar : MonoBehaviour
 {
-    //[SerializeField] private Transform player;
-    //[SerializeField] private GameObject fogOfWarPlane;
-    //[SerializeField] private LayerMask fogLayer;
-    //[SerializeField] private float radius;
-    //[SerializeField] private float radiusSqr { get { return radius * radius; } }
+    [SerializeField] private Transform player;
+    [SerializeField] private LayerMask fogLayer;
+    [SerializeField] private float radius = 20f;
+    [SerializeField] private Color initialColor;
+    private float m_radiusSqr { get { return radius * radius; } }
 
-    public GameObject m_fogOfWarPlane;
-    public Transform m_player;
-    public LayerMask m_fogLayer;
-    public float m_radius = 5f;
-    private float m_radiusSqr { get { return m_radius * m_radius; } }
+    private ProBuilderMesh ground;
+    public Material fogMaterial;
+    private Vector3[] points;
+    [SerializeField] private int UVLength = 100;
+    [SerializeField] private Vector3 center;
+    [SerializeField] private int edgeLength;
 
     private Mesh m_mesh;
     private Vector3[] m_vertices;
@@ -23,19 +26,26 @@ public class FogOfWar : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        m_mesh = ground.GetComponent<MeshFilter>().mesh;
+        m_vertices = m_mesh.vertices;
+        m_colors = new Color[m_vertices.Length];
+        for (int i = 0; i < m_colors.Length; i++)
+        {
+            m_colors[i] = initialColor;
+        }
         Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray r = new Ray(transform.position, m_player.position - transform.position);
+        Ray r = new Ray(transform.position, player.position - transform.position);
         RaycastHit hit;
-        if (Physics.Raycast(r, out hit, 1000, m_fogLayer, QueryTriggerInteraction.Collide))
+        if (Physics.Raycast(r, out hit, 10000, fogLayer, QueryTriggerInteraction.Collide))
         {
             for (int i = 0; i < m_vertices.Length; i++)
             {
-                Vector3 v = m_fogOfWarPlane.transform.TransformPoint(m_vertices[i]);
+                Vector3 v = ground.transform.TransformPoint(m_vertices[i]);
                 float dist = Vector3.SqrMagnitude(v - hit.point);
                 if (dist < m_radiusSqr)
                 {
@@ -49,14 +59,33 @@ public class FogOfWar : MonoBehaviour
 
     void Initialize()
     {
-        m_mesh = m_fogOfWarPlane.GetComponent<MeshFilter>().mesh;
-        m_vertices = m_mesh.vertices;
-        m_colors = new Color[m_vertices.Length];
-        for (int i = 0; i < m_colors.Length; i++)
+        Vector3[] points = new Vector3[UVLength * UVLength];
+        Face[] faces = new Face[(UVLength - 1) * (UVLength - 1)];
+        float xThreshold = (edgeLength) / UVLength;
+        for (int i = 0; i < points.Length; i++)
         {
-            m_colors[i] = Color.black;
+            points[i] = new Vector3(center.x + (xThreshold * (i % UVLength)), center.y, center.z + (xThreshold * (i / UVLength)));
         }
-        UpdateColor();
+        int j = 0;
+        for (int i = 0; i < points.Length - UVLength; i++)
+        {
+            if (i % UVLength != UVLength - 1)
+            {
+                faces[j] = new Face(new int[] { i, i + 1, i + UVLength, i + 1, i + UVLength + 1, i + UVLength });
+                j++;
+            }
+        }
+
+        ground = ProBuilderMesh.Create(points, faces);
+        ground.CenterPivot(new int[] { 0, UVLength - 1, UVLength * (UVLength - 1), (UVLength * UVLength) - 1 });
+        ground.Refresh();
+        MeshRenderer meshRend = ground.GetComponent<MeshRenderer>();
+        meshRend.material = fogMaterial;
+        ground.transform.position = center;
+        ground.gameObject.layer = 9;
+        ground.gameObject.name = "fog";
+        ground.gameObject.AddComponent<MeshCollider>();
+        ground.transform.Rotate(new Vector3(180f, 0, 0));
     }
 
     void UpdateColor()
