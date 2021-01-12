747d5fbe3890d7bb4034c953fb02b700b93aa542
idanro <idan.rooze@gmail.com>
2021-01-12 18:15:17 +0200

    Fixes!

--- Assets/Scripts/AimController.cs
+++ Assets/Scripts/AimController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Helpers;
 using UnityEngine;
-using UnityEngine.XR.WSA.Input;
+//using UnityEngine.XR.WSA.Input;
 
 public class AimController : ListeningMonoBehaviour
 {
