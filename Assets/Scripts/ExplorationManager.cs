98b9b1a100a2dac2771e90b24b3498667663c308
Dori Rosenberg <33037430+Dori203@users.noreply.github.com>
2021-01-16 22:40:03 +0200

    Exploration Manager - Added manager, created collider for lighthouses.

---
+++ Assets/Scripts/ExplorationManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Helpers;
+using UnityEngine;
+
+public class ExplorationManager : MonoBehaviour
+{
+
+    public static ExplorationManager SharedInstance;
+
+
+    [SerializeField] private ArrayList interetPoints = new ArrayList();
+    [SerializeField] private List<int> interetPointsEngagedList = new List<int>();
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    public void interestPointHit(int interestPointIndex) 
+    {
+        //add score to the exploration scores.
+        
+        //broadcast
+        GameManager.Instance.InterestPointHit(interestPointIndex);
+
+        //increase exploration score.
+        GameManager.Instance.incExplorationScore();
+    }
+}
