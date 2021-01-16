25a449374b42386df868805736884f0e9c6986a2
Dori Rosenberg <33037430+Dori203@users.noreply.github.com>
2021-01-17 01:16:25 +0200

    Change mosquitos to not parent with mosquitoLimit, but be rejected by it.

--- Assets/Scripts/MosquitoController.cs
+++ Assets/Scripts/MosquitoController.cs
@@ -6,6 +6,8 @@ public class MosquitoController : MonoBehaviour
 {
     [SerializeField] private float pullForce = 5.42f;
     [SerializeField] private float maxSpeed = 25f;
+    [SerializeField] private float magnitude = 0.03f;
+
     private Rigidbody rb;
     private bool inRaft = false;
     public int MosquitoeNumber;
@@ -19,7 +21,7 @@ public class MosquitoController : MonoBehaviour
     //When the Mosquito collides with the MosquitoAttractor, it will start moving towards the raft.
     private void OnTriggerStay(Collider other)
     {
-        if (!inRaft && other.tag == "MosquitoAttractor")
+        if (other.tag == "MosquitoAttractor")
         {
             Vector3 forceDirection = other.transform.position - transform.position;
             // apply force on target towards raft.
@@ -32,9 +34,12 @@ public class MosquitoController : MonoBehaviour
         if (other.tag == "MosquitoLimit")
         {
             inRaft = true;
-            this.transform.SetParent(other.transform);
-            rb.isKinematic = true;
             
+            // calculate force vector
+            var force = transform.position - other.transform.position;
+            // normalize force vector to get direction only and trim magnitude
+            force.Normalize();
+            gameObject.GetComponent<Rigidbody>().AddForce(force * magnitude,ForceMode.Impulse);
             ExterminationManager.SharedInstance.MosquitoesEngaged(MosquitoeNumber);
         }
 
