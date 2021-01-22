bd223e8e33ffbf5274c10051c4ae0cfa0af83995
asafarad1 <asafarad1@gmail.com>
2021-01-22 19:38:54 +0200

    shipMovment white Paddle

--- Assets/Scripts/movment/PlayerController.cs
+++ Assets/Scripts/movment/PlayerController.cs
@@ -3,96 +3,77 @@ using System.Collections.Generic;
 using Ditzelgames;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using Sirenix.OdinInspector;
 
-public class PlayerController : MonoBehaviour { 
-    //Drags
-    public Transform boatTransform;
+public class PlayerController : MonoBehaviour {
     private Rigidbody boatRB;
 
-    //How fast should the engine accelerate?
-    public float powerFactor;
-    public float maxSpeed;
-    public float rotationSpeed;
-    private float thrustFromWaterJet = 0f;
-    private float boatRotation_Y = 0f;
-    private float rotationDirection = -1f;
-    private bool rotating = false;
-    void Start() 
-	{
-        boatRB = GetComponent<Rigidbody>();
-        rotationDirection = -1f;
-        rotating = false;
-}
+    [BoxGroup("paddle timing")] public float restDuration = 0.7f;
 
-void Update() 
-	{
+    [BoxGroup("paddle timing")] public float PaddleDuration = 0.5f;
+
+    [HorizontalGroup("Split", 0.5f, LabelWidth = 75)] 
+    [BoxGroup("Split/rawing")] public float force = 5f;
+
+    [BoxGroup("Split/rawing")] public float backPos = 2.54f;
+
+    [BoxGroup("Split/rotate")] public float RotateMin = 0.5f;
+
+    [BoxGroup("Split/rotate")] public float RotateMax = 1.81f;
+
+    [BoxGroup("Split/rotate")] public int RotateStep = 4;
+
+    private bool paddling = false;
+    private bool PaddleRight = false;
+    private float sidePos;
+
+    private float nextActionTime = 0.0f;
+    private float stepInc = 1;
+    private bool startPaddle;
+
+    private Vector3 forwardVector3 = Vector3.forward;
+
+
+    void Start() {
+        boatRB = GetComponent<Rigidbody>();
+        // rotationDirection = -1f; // todo delete
+        paddling = false;
+        sidePos = RotateMin;
+        stepInc = (RotateMax - RotateMin) / RotateStep;
+    }
+
+    void Update() {
         UserInput();
     }
 
-    void FixedUpdate()
-    {
-        Rotate();
-        UpdateWaterJet();
+    void FixedUpdate() {
+        if (Time.time > nextActionTime) {
+            startPaddle = !startPaddle;
+            nextActionTime += startPaddle ? PaddleDuration : restDuration;
+
+            sidePos += (Mathf.Abs(sidePos) < Mathf.Abs(RotateMax)) ? stepInc : 0;
+        } else {
+            if (startPaddle && paddling) Paddle();
+        }
+    }
+
+    void Paddle() {
+        boatRB.AddForceAtPosition(boatRB.rotation * forwardVector3 * force,
+            boatRB.rotation * new Vector3(sidePos, 0, backPos) + boatRB.position,
+            ForceMode.Force);
     }
 
     void UserInput() {
         //Steer left
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            rotating = true;
-            rotationDirection *= -1f;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            paddling = true;
+            PaddleRight = !PaddleRight; // switch direction
+            stepInc = -1f * stepInc;
+            sidePos = RotateMin * Mathf.Sign(stepInc);
         }
-        if (Input.GetKeyUp(KeyCode.Alpha2))
-        {
-            rotating = false;
-        }
-        
-        // todo delete
-        if (Input.GetKeyUp(KeyCode.T))
-        {
-            GameManager.Instance.MosquitoesTriggered(1);
-            Debug.Log("Triggered!");
-        }
-        if (Input.GetKeyUp(KeyCode.Y))
-        {
-            GameManager.Instance.MosquitoesTriggered(1);
-        }
-        // todo delete
-        
-    }
 
-    void Rotate()
-    {
-        if (rotating)
-        {
-            boatRotation_Y = boatTransform.localEulerAngles.y + rotationSpeed * rotationDirection;
-            Vector3 newRotation = new Vector3(0f, boatRotation_Y, 0f);
-            boatTransform.localEulerAngles = newRotation;
+        if (Input.GetKeyUp(KeyCode.Alpha2)) {
+            paddling = false;
         }
-    }
-
-    void UpdateWaterJet()
-    {
-        PhysicsHelper.ApplyForceToReachVelocity(boatRB, boatTransform.forward * powerFactor, 1f);
-        if(boatRB.velocity.magnitude > maxSpeed){
-            boatRB.velocity = Vector3.ClampMagnitude(boatRB.velocity, maxSpeed);
-        }
-        //
-        // Debug.Log(boatController.CurrentSpeed);
-        //
-        // Vector3 forceToAdd = waterJetTransform.forward * currentJetPower;
-        //
-        // //Only add the force if the engine is below sea level
-        // // float waveYPos = WaterController.current.GetWaveYPos(waterJetTransform.position, Time.time);
-        // float waveYPos = 0.77f;
-        //
-        // if (waterJetTransform.position.y < waveYPos)
-        // {
-        //     boatRB.AddForceAtPosition(forceToAdd, waterJetTransform.position);
-        // }
-        // else
-        // {
-        //     boatRB.AddForceAtPosition(Vector3.zero, waterJetTransform.position);
-        // }
     }
 }
