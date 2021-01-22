48300c30d77ec4d8dc383c9052471b64e4f8c0b3
idanro <idan.rooze@gmail.com>
2021-01-23 01:36:05 +0200

    Vegetation bend when raft is close

---
+++ Assets/Bend.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bend : MonoBehaviour
+{
+    public float distance;
+    public float originalHeight;
+    public Transform raft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalHeight = transform.localScale.y;
+        raft = GameObject.Find("Raft").transform;
+    }
+
+    void FixedUpdate()
+    {
+        distance = Mathf.Abs((transform.position - raft.position).magnitude);
+        if (distance < 5f)
+        {
+            transform.localScale = new Vector3(transform.localScale.x,
+                                                distance/5*originalHeight,
+                                                transform.localScale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(transform.localScale.x,
+                                    originalHeight,
+                                    transform.localScale.z);
+
+        }
+    }
+}
