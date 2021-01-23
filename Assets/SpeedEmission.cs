3c07d2c61c25368641d823d22f79e1d50ac9a498
idanro <idan.rooze@gmail.com>
2021-01-23 02:16:14 +0200

    Trail appears when raft is above a certain velocity

---
+++ Assets/SpeedEmission.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedEmission : MonoBehaviour
+{
+    private float currentSpeed = 0f;
+    private ParticleSystem particleSystem;
+    public Rigidbody raftRigidBody;
+
+
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        currentSpeed = raftRigidBody.velocity.magnitude;
+        if (currentSpeed > 1f)
+        {
+            particleSystem.emissionRate = 25f;
+            Debug.Log("Emission 25");
+        }
+        else
+        {
+            particleSystem.emissionRate = 0f;
+            Debug.Log("Emisison 0");
+        }
+    }
+}
