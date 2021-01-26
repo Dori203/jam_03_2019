5f275735e4b7f8e24d442be363c8f27b11da1d12
idanro <idan.rooze@gmail.com>
2021-01-26 16:22:28 +0200

    New landmark, circular level

--- Assets/EdgeSpawner.cs
+++ Assets/EdgeSpawner.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnRandom : MonoBehaviour
+public class EdgeSpawner : MonoBehaviour
 {
+    // Start is called before the first frame update
     public GameObject[] objects;
     public int numberOfObjects = 500;
     public float squareLength = 240;
@@ -15,24 +16,34 @@ public class SpawnRandom : MonoBehaviour
     public bool randomRotationX = true;
     public bool randomRotationY = true;
     public bool randomRotationZ = true;
-    public float innerRadius = 90f;
-    public float outerRadius = 90f;
+    public float radius = 90f;
+
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < numberOfObjects; i++)
         {
-            float ratio = innerRadius / outerRadius;
-            float radius = Mathf.Sqrt(Random.Range(ratio * ratio, 1f)) * outerRadius;
-            var xz = Random.insideUnitCircle.normalized * radius;
-            var spawnPosition = new Vector3(xz.x, 0, xz.y);
+            /* Distance around the circle */
+            var radians = 2 * Mathf.PI / numberOfObjects * i;
+
+            /* Get the vector direction */
+            var vertical = Mathf.Sin(radians);
+            var horizontal = Mathf.Cos(radians);
+
+            var spawnDir = new Vector3(horizontal, 0, vertical);
+
+            /* Get the spawn position */
+            var spawnPos = spawnDir * radius; // Radius is just the distance away from the 
+
+            /* Now spawn */
             GameObject objectType = objects[Random.Range(0, objects.Length)];
-            GameObject currentObject = Instantiate(objectType, spawnPosition, Quaternion.identity, gameObject.transform);
+            GameObject currentObject = Instantiate(objectType, spawnPos, Quaternion.identity, gameObject.transform);
             currentObject.transform.localRotation = Quaternion.Euler(new Vector3(System.Convert.ToInt32(randomRotationX) * (Random.Range(0f, 360f)),
                             System.Convert.ToInt32(randomRotationY) * (Random.Range(0f, 360f)),
                             System.Convert.ToInt32(randomRotationZ) * (Random.Range(0f, 360f))));
-            float rockScale = Random.Range(rockMinScale, rockMaxScale);
-            currentObject.transform.localScale = new Vector3(rockScale, rockScale, rockScale);
+
+            float objectScale = Random.Range(rockMinScale, rockMaxScale);
+            currentObject.transform.localScale = new Vector3(objectScale, objectScale, objectScale);
         }
     }
 }
