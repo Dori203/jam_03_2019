55d5b4cb6f0fe9e11d8cea67e96c445f2f7c4b20
asafarad1 <asafarad1@gmail.com>
2021-01-26 17:27:32 +0200

    camera ar boat front

--- Assets/Scripts/KillingCamera.cs
+++ Assets/Scripts/KillingCamera.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using Helpers;
 using UnityEngine;
+
 //using UnityEngine.XR.WSA.Input;
 
 public class KillingCamera : ListeningMonoBehaviour {
     protected override List<BaseListener> Listeners => new List<BaseListener>() {
-        new BaseListener<int> {Event = GameManager.Channels.MosquitoesEngaged.GetPath(), Callback = mosquitoesEngaged },
-        new BaseListener<int> {Event = GameManager.Channels.MosquitoeHit.GetPath(), Callback = MosquitoeHit }
+        new BaseListener<int> {Event = GameManager.Channels.MosquitoesEngaged.GetPath(), Callback = mosquitoesEngaged},
+        new BaseListener<int> {Event = GameManager.Channels.MosquitoeHit.GetPath(), Callback = MosquitoeHit}
     };
 
     public float MosquitoInRangeSensitivity = 0.1f;
@@ -20,42 +21,26 @@ public class KillingCamera : ListeningMonoBehaviour {
     [SerializeField] private GameObject aim;
     [SerializeField] private bool rotateTowardsMosquitos;
 
-    
-
-
 
     private bool mosquitoesEngagedMode;
 
-    private void Awake()
-    {
+    private void Awake() {
         engagedMosquito = aim; // todo test
     }
 
-    private void LateUpdate()
-    {
+    private void LateUpdate() {
         Vector3 newPosition = player.position + positionOffset;
         transform.position = newPosition;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, player.rotation * rotationOffset, 45.0f * Time.deltaTime);
 
-        //cruising
-        if (followRotation && !mosquitoesEngagedMode)
-        {
-            //Debug.Log("CRUSING");
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, player.rotation * rotationOffset, 45.0f * Time.deltaTime);
-        //engaged in battle.
-        } else if (mosquitoesEngagedMode) {
+        if (mosquitoesEngagedMode) {
             //get closest mosquito
             Vector3 relativePos = engagedMosquito.transform.position - transform.position;
-            //Debug.Log("follow mosquito");
-            if (rotateTowardsMosquitos)
-            {
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(relativePos, Vector3.up), 45.0f * Time.deltaTime);
-            }
 
             //stop rotating once camera is close to pointing at mosquito.
             float angle = Quaternion.Angle(transform.rotation, Quaternion.LookRotation(relativePos, Vector3.up));
             GameManager.Instance.MosquitoesInCamera(angle < MosquitoInRangeSensitivity);
             aim.SetActive(angle < MosquitoInRangeSensitivity);
-
         }
     }
 
@@ -65,14 +50,14 @@ public class KillingCamera : ListeningMonoBehaviour {
             aim.SetActive(false);
             return;
         }
+
         if (!mosquitoesEngagedMode) {
-            mosquitoesEngagedMode = true; 
+            mosquitoesEngagedMode = true;
             engagedMosquito = MosquitoSpawner.SharedInstance.GetPooledObjectByIndex(MosquitoeNumber);
         }
     }
 
-    private int findNextMosquito()
-    {
+    private int findNextMosquito() {
         //calculate angles
         float minAngle = 360;
         float curAngle = 0;
@@ -80,28 +65,26 @@ public class KillingCamera : ListeningMonoBehaviour {
         int minIndex = -1;
 
         //find minimum angle mosquito
-        for (int i = 0; i < ExterminationManager.SharedInstance.getEngagedListSize(); i++)
-        {
+        for (int i = 0; i < ExterminationManager.SharedInstance.getEngagedListSize(); i++) {
             curAngle = calculateAngle(ExterminationManager.SharedInstance.getEngagedMosquitoPositionByIndex(i));
-            if(curAngle < minAngle)
-            {
+            if (curAngle < minAngle) {
                 minAngle = curAngle;
                 minIndex = ExterminationManager.SharedInstance.getEngagedMosquitoNumberByIndex(i);
             }
         }
+
         //Debug.Log("minIndex : " + minIndex);
         return minIndex;
     }
 
-    private float calculateAngle(Vector3 mosquitoPosition)
-    {
+    private float calculateAngle(Vector3 mosquitoPosition) {
         //float angle = Vector3.SignedAngle(this.transform.forward, this.transform.position - mosquitoPosition, this.transform.forward);
         //return Mathf.Abs(angle);
         float angle = Vector3.Angle(this.transform.forward, this.transform.position - mosquitoPosition);
-        if(angle > 180)
-        {
+        if (angle > 180) {
             return -1 * (angle - 180);
         }
+
         return angle;
     }
 
