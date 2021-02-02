1b2ca650a9fd63f7d673c4f161bb1eb7254c9ec3
Dori Rosenberg <33037430+Dori203@users.noreply.github.com>
2021-02-02 17:14:01 +0200

    Make raft move away from rocks

---
+++ Assets/moveAwayFromRock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveAwayFromRock : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float rockHitForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Rock"))
+        {
+            rb.AddForce(rockHitForce * (rb.position - collision.transform.position).normalized, ForceMode.Impulse);
+        }
+    }
+}
